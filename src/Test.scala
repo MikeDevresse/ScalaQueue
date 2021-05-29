3d3a3cbf456f432fcede0c41ad4f6ccca115ec09
Mike Devresse <mike.devresse@gmail.com>
2021-05-29 13:31:05 +0200

    Ajout de tests

--- src/Test.scala
+++ src/Test.scala
@@ -1,40 +1,46 @@
 object Test {
   def main(args: Array[String]): Unit = {
+    println("\n**** Tests basiques ****")
+    println("\n** QUEUE1")
     val queue1 = Queue[Int](Nil, Nil)
-    println("\n** QUEUE1 **")
     printQueue(queue1)
 
-    println("\n** TEST DEQUEUE VIDE **")
+    println("\n** TEST DEQUEUE VIDE")
     val dequeue1 = queue1.dequeue()
     println("Dequeue: " + dequeue1._1)
     printQueue(dequeue1._2)
 
     val queue2 = queue1.enqueue(1)
-    println("\n** QUEUE2 **")
+    println("\n** QUEUE2")
     printQueue(queue2)
 
     val queue3 = queue2.enqueue(2)
-    println("\n** QUEUE3 **")
+    println("\n** QUEUE3")
     printQueue(queue3)
 
     val queue4 = queue3.enqueue(3)
-    println("\n** QUEUE4 **")
+    println("\n** QUEUE4")
     printQueue(queue4)
 
-    println("\n** QUEUE5 **")
+    println("\n** QUEUE5")
     val dequeue5 = queue4.dequeue()
     println("Dequeue: " + dequeue5._1)
     printQueue(dequeue5._2)
 
     /** Ajoute 1 à chaque élément de notre liste */
-    println("\n** QUEUE6 **")
+    println("\n**** Test map ****")
     val queue6 = dequeue5._2.map(y => y + 1)
     printQueue(queue6)
 
     /** Addition de l'ensemble  */
-    println("\n** FOLD LEFT **")
+    println("\n**** Test foldLeft ****")
     printQueue(queue4)
     println(queue4.foldLeft(0)((a,b) => a+b))
+
+    println("\n**** Test instanciation listes non vides ****")
+    var queueNonVide = Queue[Int](List(2,5,6,7),List(5,6,1))
+    printQueue(queueNonVide)
+
   }
 
   def printQueue(q: Queue[Int]): Unit = {
